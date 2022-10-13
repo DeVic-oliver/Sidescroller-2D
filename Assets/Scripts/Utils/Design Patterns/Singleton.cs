efda6c2485364b86aa99ebacebb6fc89f097bd00
Victor Matheus <victormatheus183@gmail.com>
2022-10-13 20:55:25 -0300

    public Instance to private

--- Assets/Scripts/Utils/Design Patterns/Singleton.cs
+++ Assets/Scripts/Utils/Design Patterns/Singleton.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 namespace Scripts.Utils.DesignPatterns { 
     public class Singleton<T> : MonoBehaviour where T: MonoBehaviour
     {
-        public Singleton<T> Instance;
+        private Singleton<T> Instance;
         private void Awake()
         {
             if(Instance != null)
