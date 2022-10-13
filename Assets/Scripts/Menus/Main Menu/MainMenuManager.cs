0b49db4b43da4764735bffc9f366b8f58d07e2ec
Victor Matheus <victormatheus183@gmail.com>
2022-10-13 20:55:42 -0300

    Useless variable declaration deleted

--- Assets/Scripts/Menus/Main Menu/MainMenuManager.cs
+++ Assets/Scripts/Menus/Main Menu/MainMenuManager.cs
@@ -1,15 +1,8 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 public class MainMenuManager : MonoBehaviour
-{
-    [Header("Buttons")]
-    public Button StartButton;
-    public Button ExitButton;
-
-
+{ 
     public void StartGame()
     {
         SceneManager.LoadScene(1);
