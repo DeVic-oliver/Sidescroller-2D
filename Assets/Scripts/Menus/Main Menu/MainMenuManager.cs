78b400c26b3659a0cd790e9468307b187be18d55
Victor Matheus <victormatheus183@gmail.com>
2022-10-13 20:52:26 -0300

    MainMenuManager created
    Will handle the click events from main menu buttons

--- Assets/Scripts/Menus/Main Menu/MainMenuManager.cs
+++ Assets/Scripts/Menus/Main Menu/MainMenuManager.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 public class MainMenuManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Buttons")]
+    public Button StartButton;
+    public Button ExitButton;
+
+
+    public void StartGame()
     {
-        
+        SceneManager.LoadScene(1);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void QuitGame()
     {
-        
+        Application.Quit();
     }
 }
