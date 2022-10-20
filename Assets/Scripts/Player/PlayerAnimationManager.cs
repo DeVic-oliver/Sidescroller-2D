030cb87a84e0b677406689157d4a710925c48c02
Victor Matheus <victormatheus183@gmail.com>
2022-10-19 23:16:15 -0300

    scene saved

--- Assets/Scripts/Player/PlayerAnimationManager.cs
+++ Assets/Scripts/Player/PlayerAnimationManager.cs
@@ -1,18 +1,51 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerAnimationManager : MonoBehaviour
 {
+    private Animator _animator;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _animator = GetComponent<Animator>();
     }
-
-    // Update is called once per frame
-    void Update()
+    public void EnableAnimation(string animationID, bool value)
     {
-        
+        _animator.SetBool(animationID, value);
+    }
+    public void DisableAnimation(string animationID, bool value)
+    {
+        _animator.SetBool(animationID, value);
+    }
+    public void TriggerAnimation(string animationID)
+    {
+        _animator.SetTrigger(animationID);
+    }
+    public void EnableIdleAnimation()
+    {
+        _animator.SetBool("IsAlive", true);
+    }
+    public void DisableDeathAnimation()
+    {
+        _animator.SetBool("IsAlive", false);
+    }
+    public void EnableRunAnimation()
+    {
+        _animator.SetBool("IsMoving", true);
+    }
+    public void DisableRunAnimation()
+    {
+        _animator.SetBool("IsMoving", false);
+    }
+    public void TriggerJumpAnimation()
+    {
+        _animator.SetTrigger("Jump");
+    }
+    public void TriggerFallAnimation()
+    {
+        _animator.SetTrigger("Fall");
+    }
+    public void TriggerLandAnimation()
+    {
+        _animator.SetTrigger("Land");
     }
 }
