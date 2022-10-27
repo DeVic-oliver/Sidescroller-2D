9547d4fb9b5d42d1e22269fb93f454d22feb932e
Victor Matheus <victormatheus183@gmail.com>
2022-10-26 22:27:42 -0300

    Player movement no longer handle with anim actions

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -7,13 +7,6 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private PlayerData PlayerData;
     private Rigidbody2D _rigidbody;
     private float moveSpeed;
-    #region Player Movement variables
-    private bool isGrounded = false;
-    private bool isJumping = false;
-    private bool isFalling = false;
-    private bool isMoving = false;
-    private bool hasLanded = false;
-    #endregion
     // Start is called before the first frame update
     void Start()
     {
@@ -24,54 +17,28 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        CheckIfPlayerIsMoving();
         CheckJumpInput();
         ReducePlayerVelocityByFrame();
-        if(GetPlayerRigidbodyVerticalVelocity() < 0)
-        {
-            SetIsFalling(true);
-        }
-    }
-    private void CheckIfPlayerIsMoving()
-    {
-        float playerVelocity = Mathf.RoundToInt(GetPlayerRigidbodyHorizontalVelocity());
-        if (playerVelocity != 0)
-        {
-            isMoving = true;
-        }
-        else
-        { 
-            isMoving = false; 
-        }
     }
     private void CheckJumpInput()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && !isJumping)
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             _rigidbody.velocity = Vector2.up * PlayerData.JumpForce;
-            SetIsJumping(true);
         }
     }
-    private void SetIsJumping(bool value)
-    {
-        isJumping = value;
-    }
-   private void SetIsFalling(bool value)
-    {
-        isFalling = value;
-    }
-    
     private void ReducePlayerVelocityByFrame()
     {
-        if(_rigidbody.velocity.x > 0)
+        if (_rigidbody.velocity.x > 0)
         {
             _rigidbody.velocity -= new Vector2(.1f, 0);
         }
-        else if(_rigidbody.velocity.x < 0)
+        else if (_rigidbody.velocity.x < 0)
         {
             _rigidbody.velocity -= new Vector2(-.1f, 0);
         }
     }
+
     private void FixedUpdate()
     {
         CheckMovementInput();
@@ -117,47 +84,6 @@ public class PlayerMovement : MonoBehaviour
         moveSpeed = PlayerData.MoveSpeed;
     }
     /// <summary>
-    /// Checks if player is moving based on rigidbody velocity X
-    /// </summary>
-    /// <returns>true | false</returns>
-    public bool CheckIfIsPlayerMoving()
-    {
-        return isMoving;
-    }
-    /// <summary>
-    /// Checks if player has jumped based on rigidbody velocity Y
-    /// </summary>
-    /// <returns>true | false</returns>
-    public bool CheckIfPlayerIsJumping()
-    {
-        return isJumping;
-    }
-    /// <summary>
-    /// Checks if player is falling based on rigidbody velocity y
-    /// </summary>
-    /// <returns>true | false</returns>
-    public bool CheckIfIsPlayerFalling()
-    {
-        return isFalling;
-    }
-
-    /// <summary>
-    /// Checks if player is touching the ground
-    /// </summary>
-    /// <returns>true | false</returns>
-    public bool CheckIfPlayerIsOnGround()
-    {
-        return isGrounded;
-    }
-    /// <summary>
-    /// Checks if player is touching the ground
-    /// </summary>
-    /// <returns>true | false</returns>
-    public bool CheckIfPlayerLands()
-    {
-        return hasLanded;
-    }
-    /// <summary>
     /// Returns the player rigidbody velocity in X axis;
     /// </summary>
     /// <returns>a float number</returns>
@@ -172,24 +98,5 @@ public class PlayerMovement : MonoBehaviour
     public float GetPlayerRigidbodyVerticalVelocity()
     {
         return _rigidbody.velocity.y;
-    }
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("ground"))
-        {
-            SetHasLanded(true);
-            SetIsJumping(false);
-        }
-    }
-    private void OnCollisionStay2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("ground"))
-        {
-            SetHasLanded(false);
-        }
-    }
-    private void SetHasLanded(bool value)
-    {
-        hasLanded = value;
     }
 }
