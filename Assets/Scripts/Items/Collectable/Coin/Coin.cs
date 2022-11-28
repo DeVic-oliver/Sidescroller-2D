fcd83a9c2f3c83714328dc87c5e5c3e7d0a3cc2a
Victor Matheus <victormatheus183@gmail.com>
2022-11-28 20:47:52 -0300

    Coin prefab variant created
    Prefab variants folder created

--- Assets/Scripts/Items/Collectable/Coin/Coin.cs
+++ Assets/Scripts/Items/Collectable/Coin/Coin.cs
@@ -5,9 +5,11 @@ using Utils.Interfaces.Item;
 public class Coin : MonoBehaviour, ICollectable
 {
     public static int CoinsCollected = 0;
+    [SerializeField] private SOCoin coinData;
+
     public void ApplyPoint()
     {
-        CoinsCollected++;
+        CoinsCollected += coinData.CoinValue;
         Destroy(gameObject);
     }
 }
