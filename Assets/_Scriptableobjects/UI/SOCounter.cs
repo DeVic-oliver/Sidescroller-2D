d2507f24448f013995e17441fc9797e9e450b265
Victor Matheus <victormatheus183@gmail.com>
2022-11-28 21:29:15 -0300

    Scriptableobjects moved to new folder

---
+++ Assets/_Scriptableobjects/UI/SOCounter.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "SO_Counter", menuName = "ScriptableObjects/UI/Counter")]
+public class SOCounter : ScriptableObject
+{
+    public int Counter;
+}
