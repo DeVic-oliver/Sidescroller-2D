a010afcddf67ca988141f7e4f445e35442495ab2
Victor Matheus <victormatheus183@gmail.com>
2022-12-03 19:33:46 -0300

    variable renamed
    useless method removed

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -4,25 +4,26 @@ using UnityEngine;
 using Assets.Scripts.Core.Interfaces;
 public class PlayerMovement : IMoveable
 {
+    private Rigidbody2D _playerRigidbody;
     private PlayerData _playerData;
-    private Rigidbody2D _rigidbody;
     private float moveSpeed;
     private float jumpForce;
     private float runSpeed;
+
     public PlayerMovement(PlayerData playerData, Rigidbody2D rigidbody)
     {
         _playerData = playerData;
         moveSpeed = playerData.MoveSpeed;
         jumpForce = playerData.JumpForce;
         runSpeed = playerData.RunSpeed;
-        _rigidbody = rigidbody;
+        _playerRigidbody = rigidbody;
     }
     public void Move(bool isAlive)
     {
         if (isAlive)
         {
             float inputAxisValue = Input.GetAxis("Horizontal");
-            _rigidbody.velocity = new Vector2(inputAxisValue * moveSpeed, _rigidbody.velocity.y);
+            _playerRigidbody.velocity = new Vector2(inputAxisValue * moveSpeed, _playerRigidbody.velocity.y);
             RotatePlayer(inputAxisValue);
             CheckJumpInput();
             CheckRunInput();
@@ -32,18 +33,18 @@ public class PlayerMovement : IMoveable
     {
         if(axisValue < 0)
         {
-            _rigidbody.transform.eulerAngles = Vector3.up * 180f;
+            _playerRigidbody.transform.eulerAngles = Vector3.up * 180f;
         }
         else if(axisValue > 0)
         {
-            _rigidbody.transform.eulerAngles = Vector3.up * 0;
+            _playerRigidbody.transform.eulerAngles = Vector3.up * 0;
         }
     }
     private void CheckJumpInput()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            _rigidbody.velocity = Vector2.up * jumpForce;
+            _playerRigidbody.velocity = Vector2.up * jumpForce;
         }
     }
     private void CheckRunInput()
@@ -64,21 +65,5 @@ public class PlayerMovement : IMoveable
     private void SetDefaultSpeed()
     {
         moveSpeed = _playerData.MoveSpeed;
-    }
-    /// <summary>
-    /// Returns the player rigidbody velocity in X axis;
-    /// </summary>
-    /// <returns>a float number</returns>
-    public float GetPlayerRigidbodyHorizontalVelocity()
-    {
-        return _rigidbody.velocity.x;
-    }
-    /// <summary>
-    /// Returns the player rigidbody velocity in Y axis;
-    /// </summary>
-    /// <returns>a float number</returns>
-    public float GetPlayerRigidbodyVerticalVelocity()
-    {
-        return _rigidbody.velocity.y;
     }
 }
