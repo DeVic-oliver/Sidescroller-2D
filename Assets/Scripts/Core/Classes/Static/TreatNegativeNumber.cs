eb5933ca0fb89bef896f39e3b9eb1c24963cd5b2
Victor Matheus <victormatheus183@gmail.com>
2022-12-04 19:06:55 -0300

    Negative number treatment
    If negative number is passed via param it will be setted to zero else the original value returns

---
+++ Assets/Scripts/Core/Classes/Static/TreatNegativeNumber.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Core.Classes.Static
+{
+    public static class TreatNegativeNumber
+    {
+        /// <summary>
+        /// Treat negative value and returns 0 when found it
+        /// </summary>
+        /// <param name="value">Value to be passed</param>
+        /// <returns> 0 When value is negative or the original value passed</returns>
+        public static int GetTreatedValue(int value)
+        {
+            if (value <= 0)
+            {
+                return 0;
+            }
+            return value;
+        }
+    }
+}
