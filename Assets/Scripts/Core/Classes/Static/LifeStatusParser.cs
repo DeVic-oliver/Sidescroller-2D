0fef1b50df2a4301fc3fe6283a076f7bda1bdd15
Victor Matheus <victormatheus183@gmail.com>
2022-12-04 19:05:18 -0300

    The life status parses created
    Returns gameobject life status (alive or dead) depending on its health points

---
+++ Assets/Scripts/Core/Classes/Static/LifeStatusParser.cs
@@ -0,0 +1,19 @@
+﻿namespace Assets.Scripts.Core.Classes.Static
+{
+   public static class LifeStatusParser
+    {
+        /// <summary>
+        /// Parsers the life status of a gameo bject that have health points and returns true or false if it sill health points
+        /// </summary>
+        /// <param name="healthPoints">The health points of the game object</param>
+        /// <returns>True if still alive | False if health points equals 0</returns>
+        public static bool GetLifeStatusBasedOnHealth(int healthPoints)
+        {
+            if (healthPoints <= 0)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
