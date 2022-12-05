d0b0e3cf9a7fcc766dd91154b0f82f4b1b94cd80
Victor Matheus <victormatheus183@gmail.com>
2022-12-04 21:50:10 -0300

    Particle system
    Coin item now have a particle system that fires when is collected by player

--- Assets/Scripts/Items/Collectable/Coin/Coin.cs
+++ Assets/Scripts/Items/Collectable/Coin/Coin.cs
@@ -2,14 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Assets.Scripts.Core.Interfaces;
+
 public class Coin : MonoBehaviour, ICollectable
 {
     public static int CoinsCollected = 0;
     [SerializeField] private SOCoin soCoin;
-
+    private ParticleSystem _particleSystem;
+    private SpriteRenderer _spriteRenderer;
+    private Collider2D _collider;
+    private float secondsToDisable = 2f;
+    private void Start()
+    {
+        _particleSystem = GetComponent<ParticleSystem>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _collider = GetComponent<CircleCollider2D>();
+    }
     public void ApplyPoint()
     {
         CoinsCollected += soCoin.CoinValue;
-        Destroy(gameObject);
+        _collider.enabled = false;
+        _spriteRenderer.enabled = false;
+        _particleSystem.Play();
+        StartCoroutine("DisableMe");
+    }
+    private IEnumerator DisableMe()
+    {
+        yield return new WaitForSeconds(secondsToDisable);
+        gameObject.SetActive(true);
     }
 }
