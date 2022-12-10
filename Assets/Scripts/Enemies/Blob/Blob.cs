3efbf605e6d02543bb16c99a352293134f7aa526
Victor Matheus <victormatheus183@gmail.com>
2022-12-10 16:44:27 -0300

    basic refactor enemy

--- Assets/Scripts/Enemies/Blob/Blob.cs
+++ Assets/Scripts/Enemies/Blob/Blob.cs
@@ -6,27 +6,47 @@ public class Blob : MonoBehaviour, IDamageable
 {
     public bool IsAlive { get; set; }
     public int HealthPoints { get; private set; }
+
     [SerializeField] private BlobData blobData;
+    [SerializeField] private Color damagedColor;
+    private Color normalColor = Color.white;
+    
     private IMoveable blobMovements;
+    private SpriteRenderer _spriteRenderer;
+    private string childName = "Body";
+    private Animator _animator;
+
+
     public void ApplyDamage(int damageValue)
     {
         int damage = TreatNegativeNumber.GetTreatedValue(damageValue);
-        if(damage >= HealthPoints)
+        StartCoroutine(ApplyDamageEffect());
+        if (damage >= HealthPoints)
         {
             HealthPoints = 0;
         }
         HealthPoints -= damage;
     }
+    private IEnumerator ApplyDamageEffect()
+    {
+        _spriteRenderer.material.color = Color.Lerp(normalColor, damagedColor, 1);
+        yield return new WaitForSeconds(.1f);
+        _spriteRenderer.material.color = Color.Lerp(damagedColor, normalColor, 1);
+    }
 
     void Start()
     {
+        InitObjects();
+        _spriteRenderer = transform.Find(childName).gameObject.GetComponent<SpriteRenderer>();
         HealthPoints = blobData.HealthPoints;
         IsAlive = true;
-        InitObjects();
+        _animator.SetBool("IsAlive", true);
     }
     private void InitObjects()
     {
         blobMovements = new BlobMovement(blobData.MoveSpeed, blobData.MinMoveRangeLimit, blobData.MaxMoveRangeLimit, transform);
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -34,5 +54,25 @@ public class Blob : MonoBehaviour, IDamageable
     {
         IsAlive = LifeStatusParser.GetLifeStatusBasedOnHealth(HealthPoints);
         blobMovements.Move(IsAlive);
+
+        if (!IsAlive)
+        {
+            _animator.SetBool("IsAlive", false);
+            GetComponent<Collider2D>().enabled = false;
+            Invoke("DisableGameObject", 3f);
+        }
+    }
+    private void DisableGameObject()
+    {
+        gameObject.SetActive(false);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        IDamageable damageable = collision.gameObject.GetComponent<IDamageable>();
+        if (damageable != null)
+        {
+            _animator.SetTrigger("Attack");
+            damageable.ApplyDamage(2);
+        }
     }
 }
