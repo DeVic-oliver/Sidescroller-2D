18e83977a4771dbdb4574c04ff7711d15b1b1ff3
Victor Matheus <victormatheus183@gmail.com>
2022-12-14 19:58:46 -0300

    simple script for player jump audio

---
+++ Assets/Scripts/Player/PlayerJumpAudioPlay.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Player
+{
+    public class PlayerJumpAudioPlay : MonoBehaviour
+    {
+        [SerializeField] private AudioSource _audioSource;
+
+        private bool _audioPlayed;
+
+        private void Update()
+        {
+            if(GetComponent<Rigidbody2D>().velocity.y > 0 && !_audioPlayed)
+            {
+                _audioPlayed = true;
+                PlayAudio();
+            }
+
+            if(GetComponent<Rigidbody2D>().velocity.y == 0 && _audioPlayed)
+            {
+                _audioPlayed = false;
+            }
+        }
+
+        public void PlayAudio()
+        {
+            _audioSource.Play();
+        }
+    }
+}
